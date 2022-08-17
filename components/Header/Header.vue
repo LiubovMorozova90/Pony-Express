1412acfd7b321b53b7c68013e2dc62e981fcc894
LiubovMorozova90 <liubov.morozova.90@mail.ru>
2022-08-17 12:38:55 +0300

    1

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="header">
         <div class="logo">
-
+        </div>
+        <div class="navigation">
+            <ul>
+                <li>Адреса</li>
+                <li>Магазины</li>
+                <li>Как это работает</li>
+                <li>Тарифы</li>
+                <li>Услуги</li>
+                <li>Доставка</li>
+            </ul>
+        </div>
+        <div>
+            <button class="btn-personal-area">Личный кабинет</button>
         </div>
     </div>
 </template>
@@ -14,8 +26,8 @@ export default {
 </script>
 
 <style scoped>
-.header {
-    @apply bg-[url('/img/header-fon.png')]";
+.btn-personal-area {
+    @apply font-bold text-[16px] leading-6 text-center font-['Circe'];
 };
 
 </style>
