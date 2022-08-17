536514daeea13158d07398a87c96104af8a11a6e
LiubovMorozova90 <liubov.morozova.90@mail.ru>
2022-08-17 14:18:26 +0300

    1

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <div class="flex justify-between items-center mt-7 mx-20 mb-30">
+    <div class="mx-20">
+        <div class="flex justify-between items-center mt-7 mb-30">
             <div class="logo">
                 <img v-bind:src="require('@/static/img/header-navigation-logo.png')" />
             </div>
@@ -20,8 +20,12 @@
                 </button>
             </div>
         </div>
-        <div>
-
+        <div class="flex flex-col justify-start mb-20">
+            <p class="uppercase font-extrabold text-7.5xl tracking-wide max-w-2xl mb-12">зарубежный sale</p>
+            <p class="font-bold text-2.5xl leading-7 max-w-xs mb-46">Выгодные онлайн покупки за рубежом с быстрой доставкой</p>
+            <button class="btn-title">
+                <p class="btn-title-text">Получить адрес за рубежом</p>
+            </button>
         </div>
     </div>
     
@@ -40,5 +44,11 @@ export default {
 };
 .btn-personal-area {
     @apply text-base text-center; 
+};
+.btn-title {
+    @apply border-0 rounded-full font-bold text-center px-4 py-2;
+};
+.btn-title-text {
+    @apply text-center text-2xl;
 };
 </style>
