2f02deba2fc651e946491c882e5d796152edad86
LiubovMorozova90 <liubov.morozova.90@mail.ru>
2022-08-18 12:47:30 +0300

    1

--- components/Landing-main/Landing-main.vue
+++ components/Landing-main/Landing-main.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>
-        <div class="info-delivery">
-
-        </div>
-    </div>
+    <main>
+        <div class="px-20">
+           <div class="info-delivery">
+                <div class="flex flex-col justify-start absolute pb-20 ">
+                    <p class="font-extrabold text-7xl tracking-wide  max-w-lg mb-12">Предоставляем адреса в США, странах Европы и Японии</p>
+                    <p class="font-bold text-2xl leading-8 max-w-md">Покупайте в онлайн-магазинах США, Европы и Японии и отправляйте на свой адрес в любой город России</p>
+                </div>
+                <div>
+                    <img v-bind:src="require('@/static/img/info-delivery-1.png')" />
+                    <img v-bind:src="require('@/static/img/infj-delivery-2.png')" />
+                </div>
+                <div></div>
+            </div> 
+        </div>        
+    </main>
 </template>
 
 <style scoped>
 .info-delivery {
-    background-image: url('static/img/1.jpg');;
+    background: linear-gradient(270.19deg, #45A145 -0.01%, #71BC46 98.04%);
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="font-circe">
-    <Header ></Header>
+    <Header></Header>
     <LandingMain></LandingMain>
   </div>  
 </template>
