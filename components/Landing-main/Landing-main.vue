72d3377e5005bbaa73dada86e9c0521d08b479ed
LiubovMorozova90 <liubov.morozova.90@mail.ru>
2022-08-18 12:15:07 +0300

    1

--- components/Landing-main/Landing-main.vue
+++ components/Landing-main/Landing-main.vue
@@ -1,13 +1,12 @@
 <template>
     <div>
         <div>
-            <div class="bg-black h-90">9+56145</div>
-            <div class="bg-greenbasic">9+5321</div>
-            <div class="bg-black">8945641</div>
-            <div class="bg-greenbasic">5641</div>
-            <div class="bg-black">98645</div>
-            <div class="bg-greenbasic">654</div>
+            
         </div>
         
     </div>    
 </template>
+
+<style scoped>
+
+</style>
