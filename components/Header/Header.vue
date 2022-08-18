cd50371b650115f868a71d573c296df884de8b65
LiubovMorozova90 <liubov.morozova.90@mail.ru>
2022-08-18 11:16:52 +0300

    1

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,11 +1,8 @@
 <template>
     <header class="header">
        <div class="px-20">
-        <div class="flex justify-between items-center pt-7 mb-32">
-            <div>
+            <div class="flex justify-between items-center pt-7 mb-32">            
                 <img v-bind:src="require('@/static/img/header-navigation-logo.svg')" />
-            </div>
-            <div>
                 <ul class="font-bold text-lg">
                     <li class="navbar"><a href="#">Адреса</a></li>
                     <li class="navbar"><a href="#">Магазины</a></li>
@@ -13,36 +10,31 @@
                     <li class="navbar"><a href="#">Тарифы</a></li>
                     <li class="navbar"><a href="#">Услуги</a></li>
                     <li class="inline-block"><a href="#">Доставка</a></li>
-                </ul>
-            </div>
-            <div class="text-greenbasic">
+                </ul> 
                 <button class="btn-personal-area">
-                    <p class="btn-personal-area-text">Личный кабинет</p>
+                    <span class="btn-personal-area-text">Личный кабинет</span>
+                </button>            
+            </div>
+            <div class="flex flex-col justify-start pb-20">
+                <p class="uppercase font-extrabold leading-23 text-7xl tracking-wide max-w-2xl mb-12">зарубежный sale</p>
+                <p class="font-bold text-2xl max-w-xs mb-44">Выгодные онлайн покупки за рубежом с быстрой доставкой</p>
+                <button class="btn-title">
+                    <span class="btn-title-text">Получить адрес за рубежом</span>
                 </button>
             </div>
-        </div>
-        <div class="flex flex-col justify-start pb-20">
-            <p class="uppercase font-extrabold leading-23 text-7xl tracking-wide max-w-2xl mb-12">зарубежный sale</p>
-            <p class="font-bold text-2xl max-w-xs mb-44">Выгодные онлайн покупки за рубежом с быстрой доставкой</p>
-            <button class="btn-title">
-                <p class="btn-title-text">Получить адрес за рубежом</p>
-            </button>
-        </div>
-        <div class="flex justify-between items-start pb-32">
-            <div v-for="(benefit, index) in benefits" :key="`benefit-${index}`" class="advantage w-1/4">
-                <div class="w-22 h-22">
-                    <img :src="benefit.img" />
+            <div class="flex justify-between items-start pb-32">
+                <div v-for="(benefit, index) in benefits" :key="`benefit-${index}`" class="advantage w-1/4">
+                    <div class="w-22 h-22">
+                        <img :src="benefit.img" />
+                    </div>
+                    <div class="flex flex-col">
+                        <span v-html="benefit.title" class="flex flex-col font-bold text-2xl leading-7 mb-3" />
+                        <span class="font-normal text-lg leading-6 max-w-xs">{{ benefit.description }}</span>
+                    </div>                
                 </div>
-                <div class="flex flex-col">
-                    <span v-html="benefit.title" class="flex flex-col font-bold text-2xl leading-7 mb-3" />
-                    <span class="font-normal text-lg leading-6 max-w-xs">{{ benefit.description }}</span>
-                </div>                
             </div>
-        </div>
-    </div> 
-    </header>
-    
-    
+        </div> 
+    </header> 
 </template>
 
 <script>
@@ -81,7 +73,7 @@ export default {
 }
 
 .btn-personal-area-text {
-    @apply text-base text-center; 
+    @apply text-base text-center text-greenbasic; 
 }
 
 .btn-title {
