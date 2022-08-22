878a9ad254b70462e5fa827f290593882f673db9
LiubovMorozova90 <liubov.morozova.90@mail.ru>
2022-08-22 17:44:54 +0300

    header

--- components/LandingHeader/LandingHeader.vue
+++ components/LandingHeader/LandingHeader.vue
@@ -5,9 +5,7 @@
             <div class="flex flex-col justify-start pb-20">
                 <p class="uppercase font-extrabold leading-23 text-7xl tracking-wide max-w-2xl mb-12">зарубежный sale</p>
                 <p class="font-bold text-2xl max-w-xs mb-44">Выгодные онлайн покупки за рубежом с быстрой доставкой</p>
-                <button class="btn-title">
-                    <span class="btn-title-text">Получить адрес за рубежом</span>
-                </button>
+                <button class="btn text-xl leading-8 text-white bg-greenbasic max-w-xs ">Получить адрес за рубежом</button>
             </div>
             <LandingHeaderBenefits />
         </div> 
@@ -29,12 +27,8 @@ export default {
     background-size: cover;
 }
 
-.btn-title {
-    @apply font-bold text-center border-0 rounded-full bg-greenbasic max-w-xs px-4 py-2;
-}
-
-.btn-title-text {
-    @apply text-center text-xl leading-8 text-white;
+.btn {
+    @apply font-bold text-center rounded-full px-4 py-2;
 }
 
 </style>
--- components/LandingHeader/LandingHeaderNavigation.vue
+++ components/LandingHeader/LandingHeaderNavigation.vue
@@ -4,9 +4,7 @@
         <ul v-for="nav in navigations" :key="nav.id" class="font-bold text-lg z-50">
             <li class="navbar"><a href="#">{{nav.title}}</a></li>
         </ul> 
-        <button class="btn-personal-area z-50">
-            <span class="btn-personal-area-text">Личный кабинет</span>
-        </button>            
+        <button class="btn border text-greenbasic border-greenbasic z-50">Личный кабинет</button>            
     </div>
 </template>
 
@@ -31,12 +29,8 @@ export default {
     @apply inline-block mr-12;
 }
 
-.btn-personal-area {
-    @apply text-center font-bold border border-greenbasic rounded-full px-4 py-2;
-}
-
-.btn-personal-area-text {
-    @apply text-base text-center text-greenbasic; 
+.btn {
+    @apply text-center text-base font-bold rounded-full px-4 py-2;
 }
 
 </style>
