0f59fced6d37f14419ff116ff214b56aae9ec806
LiubovMorozova90 <liubov.morozova.90@mail.ru>
2022-08-22 16:43:12 +0300

    1

--- components/LandingMain/LandingMain.vue
+++ components/LandingMain/LandingMain.vue
@@ -26,15 +26,7 @@
                 </button>
             </div>
             <div class="flex flex-col items-center z-50">
-                <div class="max-w-7xl mb-10">
-                    <client-only>
-                        <agile ref="carousel" :options="sliderOptions">
-                            <div v-for="shop in shops" :key="shop.id" class="cursor-pointer w-80 h-64 mb-6">                        
-                                <img :src="shop.img" class="" />                      
-                            </div>
-                        </agile>
-                    </client-only>
-                </div>                    
+                <LandingMainSlider />                    
                 <button class="text-center rounded-full bg-lightgreen px-4 py-2">
                     <span class="font-bold text-base text-center text-white">Все магазины</span>
                 </button>
@@ -136,29 +128,16 @@
 <script>
 
 import LandingMainDropDown from '~/components/LandingMain/LandingMainDropdown.vue';
-import VolumeWeights from '~/components/LandingMain/VolumeWeights.vue'
+import VolumeWeights from '~/components/LandingMain/VolumeWeights.vue';
+import LandingMainSlide from './LandingMainSlider.vue';
 
 export default {
     components: {
     LandingMainDropDown,
     VolumeWeights,
+    LandingMainSlide
 },
-    data: () => ({        
-        sliderOptions: {
-        autoplay: false,
-        centerMode: true,
-        dots: true,
-        navButtons: false,
-        slidesToShow: 4,
-      },    
-        shops: [
-            {img: require('~/static/img/slider-shop-amazon.svg')},
-            {img: require('~/static/img/slider-shop-ebay.svg')},
-            {img: require('~/static/img/slider-shop-levi.svg')},
-            {img: require('~/static/img/slider-shop-6pm.svg')},
-            {img: require('~/static/img/slider-shop-amazon.svg')},
-            {img: require('~/static/img/slider-shop-amazon.svg')},
-        ],        
+    data: () => ({         
         forwardings: [
             {description: 'Цена в России «кусается», а при заказе из другой страны товар не доставляют на Ваш домашний адрес?', 
             img: require('~/static/img/info-forwarding-1.svg')},
@@ -236,52 +215,4 @@ export default {
     padding-right: 12px;
 }
 	
-</style>
-
-<style>
-.agile {
-    width: 100%;
-}
-
-.agile__actions {
-    margin-top: 20px;
-}
-
-.agile__dot {
-    margin: 0 10px;
-}
-
-.agile__dots {
-    bottom: 10px;
-	left: 50%;
-	position: absolute;
-	transform: translateX(-50%);
-}
-
-.agile__dot button {
-    background-color: transparent;
-    background-color: #326544;
-	border-radius: 50%;
-	cursor: pointer;
-	display: block;
-	height: 16px;
-	font-size: 0;
-	line-height: 0;
-	margin: 0;
-	padding: 0;
-	transition-duration: .1s;
-	width: 16px;
-}
-
-.agile__dot button:hover,
-.agile__dot button:active {    
-    background-color: #6CBE37;
-}
-
-.agile__dot--current,
-.agile__dot--current:hover,
-.agile__dot--current button {
-    background-color: #6CBE37;    
-	border-radius: 50%;
-}
 </style>
